05404d0a33d6e5ccbc485e478aadccc43a882a7d
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2026-03-26 12:09:47 +0200

    chorce: configure local postgresql for dev

---
+++ prisma/migrations/20260326100559_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "MigrationBaseline" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "note" TEXT NOT NULL,
+
+    CONSTRAINT "MigrationBaseline_pkey" PRIMARY KEY ("id")
+);
